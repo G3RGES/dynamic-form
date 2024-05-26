196eee805334b39c5e1d29395b1ed935dd490215
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-05-26 20:16:39 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,25 +5,74 @@ function App() {
     <div className="">
       <h1 className="text-3xl text-center my-4 py-2">React Forms</h1>
       <form className="w-5/6 max-w-md mx-auto">
-        <fieldset className="flex flex-col border py-3 px-4">
-          <legend className="text-2xl font-semibold">About You</legend>
-          <label className="text-3xl font-semibold">What's Your Name</label>
-          <input
-            className="border rounded text-lg leading-tight py-3 px-2 mt-4 focus:outline-indigo-500"
-            id="firstName"
-            name="firstName"
-            placeholder="First Name"
-            type="text"
-            onChange={handleInput}
-          />
-          <input
-            className="border rounded text-lg leading-tight py-3 px-2 mt-4 focus:outline-indigo-500"
-            id="lastName"
-            name="lastName"
-            placeholder="Last Name"
-            type="text"
-            onChange={handleInput}
-          />
+        <fieldset className="flex flex-col gap-2 border py-3 px-4">
+          <legend className="text-2xl font-semibold mb-2 text-gray-500">
+            About You
+          </legend>
+          <div className="">
+            <label className="text-3xl font-semibold">What's Your Name</label>
+            <input
+              className="border rounded text-lg leading-tight py-3 px-2 mt-4
+             focus:outline-indigo-500 w-3/5"
+              id="firstName"
+              name="firstName"
+              placeholder="First Name"
+              type="text"
+              onChange={handleInput}
+            />
+
+            <input
+              className="border rounded text-lg leading-tight py-3 px-2 mb-3 mt-4
+             focus:outline-indigo-500 w-3/5"
+              id="lastName"
+              name="lastName"
+              placeholder="Last Name"
+              type="text"
+              onChange={handleInput}
+            />
+          </div>
+          <div className="">
+            <label className="text-3xl font-semibold">What's Your Email?</label>
+            <input
+              className="border rounded text-lg leading-tight py-3 px-2 mb-3 mt-4
+             focus:outline-indigo-500 w-3/5"
+              type="email"
+              id="email"
+              name="email"
+              placeholder="email@example.com"
+              onChange={handleInput}
+            />
+          </div>
+          <div className="">
+            <label className="text-3xl font-semibold">
+              What's Your Birthday?
+            </label>
+            <input
+              className="border rounded text-lg leading-tight py-3 px-2 mb-3 mt-4
+             focus:outline-indigo-500 w-3/5"
+              type="date"
+              id="dob"
+              name="dob"
+              placeholder="Date of Birth"
+              max="2005-01-31"
+              onChange={handleInput}
+            />
+          </div>
+          <div className="flex flex-col gap-2">
+            <label className="text-3xl font-semibold">What's Your Gender</label>
+            <select
+              className="border rounded text-lg leading-tight py-3 px-2 mb-3 mt-4
+             focus:outline-indigo-500 w-3/5"
+              name="gender"
+              id="gender"
+              placeholder=""
+              onChange={handleInput}
+            >
+              <option className="">Gender</option>
+              <option value="male">Male</option>
+              <option value="female">Female</option>
+            </select>
+          </div>
         </fieldset>
       </form>
     </div>
