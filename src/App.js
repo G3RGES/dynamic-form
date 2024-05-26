156d5d1b87db870aef5690a5bf0a9dc5769fa6ae
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-05-26 19:54:21 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
 function App() {
+  const handleInput = () => {};
+
   return (
     <div className="">
-      <h1 className="text-3xl text-center my-2 py-2">React Forms</h1>
+      <h1 className="text-3xl text-center my-4 py-2">React Forms</h1>
+      <form className="w-5/6 max-w-md mx-auto">
+        <fieldset className="flex flex-col border py-3 px-4">
+          <legend className="text-2xl font-semibold">About You</legend>
+          <label className="text-3xl font-semibold">What's Your Name</label>
+          <input
+            className="border rounded text-lg leading-tight py-3 px-2 mt-4 focus:outline-indigo-500"
+            id="firstName"
+            name="firstName"
+            placeholder="First Name"
+            type="text"
+            onChange={handleInput}
+          />
+          <input
+            className="border rounded text-lg leading-tight py-3 px-2 mt-4 focus:outline-indigo-500"
+            id="lastName"
+            name="lastName"
+            placeholder="Last Name"
+            type="text"
+            onChange={handleInput}
+          />
+        </fieldset>
+      </form>
     </div>
   );
 }
